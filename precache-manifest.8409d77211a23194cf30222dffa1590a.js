426cad43dfbcd51a61ee00615afeca121fb418bb
VictoriaKaa <victoriaklimova96@gmail.com>
2020-02-07 21:07:43 +0300

    Updates

---
+++ precache-manifest.8409d77211a23194cf30222dffa1590a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41b95fde0991242826ece28f85268a25",
+    "url": "/Todo-List/index.html"
+  },
+  {
+    "revision": "e2caa18ab6b5318d1644",
+    "url": "/Todo-List/static/css/main.796de893.chunk.css"
+  },
+  {
+    "revision": "b630b3f919fb119cb31d",
+    "url": "/Todo-List/static/js/2.5dfabf45.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Todo-List/static/js/2.5dfabf45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2caa18ab6b5318d1644",
+    "url": "/Todo-List/static/js/main.85a0a62f.chunk.js"
+  },
+  {
+    "revision": "9c005fce0e1c32787390",
+    "url": "/Todo-List/static/js/runtime-main.dc3950e3.js"
+  },
+  {
+    "revision": "112e1affa169aee3b42b6ad1e60a1e54",
+    "url": "/Todo-List/static/media/icomoon.112e1aff.eot"
+  },
+  {
+    "revision": "a455d0946fe52cd8467a56e0c1e9a00d",
+    "url": "/Todo-List/static/media/icomoon.a455d094.svg"
+  },
+  {
+    "revision": "c14fad96a501afa3c06adbc23a725bb0",
+    "url": "/Todo-List/static/media/icomoon.c14fad96.woff"
+  },
+  {
+    "revision": "f5d95101e3c4df3c50c4c0cf54d887a6",
+    "url": "/Todo-List/static/media/icomoon.f5d95101.ttf"
+  }
+]);
